3f8b5c30ee377491918ce0418247bf7b37421896
Anastasia Bushina <65818263+shhtol@users.noreply.github.com>
2020-07-29 11:27:42 +0300

    Update GenericPlayer.cpp

--- GenericPlayer.cpp
+++ GenericPlayer.cpp
@@ -16,4 +16,24 @@ class GenericPlayer : public Hand{
         if(IsBoosted()) cout << name << ": Boosted!" << endl; 
     }
     
+    friend ostream& operator<<(ostream& os, const GenericPlayer& g){
+        os << g.m_Name « ":\t";
+        vector<Card*>::const_iterator pCard;
+        if (!g.m_Саrds.empty())
+        {
+            for (pCard = g.m_Cards.begin(); pCard != g.m_Cards.end(); pCard++) {
+            os << *(*pCard) << "\t";
+            }
+          if (g.GetTotal() != 0) {
+          cout << g.GetTotal() << endl;
+          }
+        }
+         else
+         {
+             os << "Empty" << endl;
+         }
+        return os;
+    }
+
+
 };
