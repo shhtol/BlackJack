951a656ab792045ec6358fa0125ace4e728c1d92
Anastasia Bushina <65818263+shhtol@users.noreply.github.com>
2020-08-08 15:04:59 +0300

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -1,10 +1,3 @@
-#include <vector>
-#include <string>
-#include <iostream>
-#include <stdlib.h>
-#include <time.h>
-using namespace std;
-
 class Game {
     private:
     vector <Player> players;
@@ -15,22 +8,22 @@ class Game {
     vector<string>::const_iterator pName;
          for (pName = names.begin(); pName != names.end(); pName++)
             {
-                    players.push_back(Player(*pName)):
+                    players.push_back(Player(*pName));
             }
 
-        srand(static cast<unsigned int>(time(O))):
-        deck.Populate();
-        deck.Shuffle();
+        srand(static_cast<unsigned int>(time(0)));
+        desk.Populate();
+        desk.Shuffle();
     }
     
-    void  Plау()
+    void Plау()
     {
        vector<Player>::iterator pPlayer;
         for (int i = 0; i < 2; i++)
             {
-                   for (pPlayer = m_Players.begin(); pPlayer != m_Players.end(); pPlayer++)
+                   for (pPlayer = players.begin(); pPlayer != players.end(); pPlayer++)
                     {
-                        desk.Deal(*pPlayer):
+                        desk.Deal(*pPlayer);
                     }
                 desk.Deal(house);
             }
@@ -45,28 +38,25 @@ class Game {
 
         for (pPlayer = players.begin(); pPlayer != players.end(); pPlayer++)
             {
-                deck.AdditionalCards(*pPlayer);
+                desk.AdditionalCards(*pPlayer);
             }
 
         house.FlipFirstCard();
         cout << endl;
         cout << house << endl;
-        deck.AdditionalCards(house):
-        if (house.IsBusted())
+        desk.AdditionalCards(house);
+        if (house.IsBoosted())
                 {
                   for (pPlayer = players.begin(); pPlayer != players.end(); pPlayer++){
-                     if (!(pPlayer->IsBusted())
+                     if (!(pPlayer->IsBoosted()))
                        {
-                             pPlayer->Win();
-                           
+                             pPlayer->Win();   
                        }
                   }
-                }
-        else
-            {
+        }else{
                 for (pPlayer = players.begin(); pPlayer != players.end(); pPlayer++)
                     {
-                        if (!(pPlayer->IsBusted()))
+                        if (!(pPlayer->IsBoosted()))
                             {
                                 if (pPlayer->GetTotal() > house.GetTotal())
                                     {
@@ -85,13 +75,11 @@ class Game {
             }
         for (pPlayer = players.begin(); pPlayer != players.end(); pPlayer++)
             {
-                pPlayer->Clear():
+                pPlayer->Clear();
             }
         house.Clear();
     }
 
     
 };
-    
-
 
